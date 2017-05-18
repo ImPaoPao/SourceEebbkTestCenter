d5ec4f260e6bb3e636efe19b6fd1a0af3cad171a
zhaotingting <zhaotingting@oaserver.dw.gdbbk.com>
2017-05-18 15:46:11 +0800

    修改module名字

--- performance-test/src/main/java/com/eebbk/test/performance/PerforTestCase.java
+++ performance-test/src/main/java/com/eebbk/test/performance/PerforTestCase.java
@@ -94,6 +94,8 @@ public class PerforTestCase extends Automator {
         }
         mStartTime = null;
     }
+
+
     public void stopTestRecord(String loadtime) {
         Log.i(TAG, "record endtime and infos");
         if (mStartTime != null) {
@@ -102,6 +104,26 @@ public class PerforTestCase extends Automator {
                 mXml.startTag(null, "Segment");
                 mXml.attribute(null, "starttime", mStartTime);
                 mXml.attribute(null, "loadtime", loadtime);
+                mXml.attribute(null, "endtime", getCurrentDate());
+                mXml.endTag(null, "Segment");
+            } catch (IOException e) {
+                // Nothing to do
+            }
+        }
+        mStartTime = null;
+    }
+    public void stopTestRecord(String loadtime, String startScreen, String endScreen, String compareTime, String compareResult) {
+        Log.i(TAG, "record endtime and infos");
+        if (mStartTime != null) {
+            try {
+                mXml.text("\n  ");
+                mXml.startTag(null, "Segment");
+                mXml.attribute(null, "starttime", mStartTime);
+                mXml.attribute(null, "loadtime", loadtime);
+                mXml.attribute(null, "startScreen", startScreen);
+                mXml.attribute(null, "endScreen", endScreen);
+                mXml.attribute(null, "compareTime", compareTime);
+                mXml.attribute(null, "compareResult", compareResult);
                 mXml.attribute(null, "endtime", getCurrentDate());
                 mXml.endTag(null, "Segment");
             } catch (IOException e) {
--- performance-test/src/main/java/com/eebbk/test/performance/SyncChineseTestCase.java
+++ performance-test/src/main/java/com/eebbk/test/performance/SyncChineseTestCase.java
@@ -2,6 +2,7 @@ package com.eebbk.test.performance;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.RemoteException;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.uiautomator.By;
 import android.support.test.uiautomator.BySelector;
@@ -13,7 +14,6 @@ import com.eebbk.test.common.BitmapHelper;
 import com.eebbk.test.common.PackageConstants.SynChinese;
 
 import org.json.JSONException;
-import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -25,11 +25,14 @@ import static android.os.SystemClock.sleep;
 @RunWith(AndroidJUnit4.class)
 public class SyncChineseTestCase extends PerforTestCase {
     @Test
-    public void launchSyncChinese() throws IOException, UiObjectNotFoundException, JSONException {
+    public void launchSyncChinese() throws IOException, UiObjectNotFoundException, JSONException, RemoteException {
         //mType 0:冷启动
         BySelector synchinese = By.text("同步语文");
         for (int i = 0; i < mCount; i++) {
-            JSONObject obj = new JSONObject();
+            String startScreen;
+            String endScreen;
+            String compareTime;
+            int compareResult = 10;
             swipeCurrentLauncher();
             mDevice.wait(Until.hasObject(By.text("同步语文")), WAIT_TIME);
             UiObject2 synChineseObj = mDevice.findObject(synchinese);
@@ -38,27 +41,21 @@ public class SyncChineseTestCase extends PerforTestCase {
             startTestRecord();
             //将源图片转换为Bitmap
             synChineseObj.clickAndWait(Until.newWindow(), WAIT_TIME);
-            int compare_result = 10;
-            int m = 3;
             do {
-                m++;
-                obj.put("startScreen" + String.valueOf(m), getCurrentDate());
+                startScreen = getCurrentDate();
                 Bitmap des_png = mAutomation.takeScreenshot();
-                obj.put("endScreen" + String.valueOf(m), getCurrentDate());
-                compare_result = BitmapHelper.compare(source_png, des_png);
-                obj.put("compareTime" + String.valueOf(m), getCurrentDate());
-                obj.put("compare_result:" + String.valueOf(m), compare_result);
-//                stopTime=getCurrentDate();
-            } while (compare_result >= 10);
-            String stopTime=getCurrentDate();
-            obj.put("stopTime:", stopTime);
-            instrumentationStatusOut(obj);
+                endScreen = getCurrentDate();
+                compareResult = BitmapHelper.compare(source_png, des_png);
+                compareTime = getCurrentDate();
+            } while (compareResult >= 10);
+            String loadTime = getCurrentDate();
             //button
             mDevice.wait(Until.hasObject(By.res(SynChinese.PACKAGE, "refresh")), WAIT_TIME);
-            stopTestRecord(stopTime);
+            stopTestRecord(loadTime, startScreen, endScreen, compareTime, String.valueOf(compareResult));
             mDevice.pressHome();
             clearRunprocess();
             sleep(1000);
+
         }
     }
 }
