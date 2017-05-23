117bdb854d65e16a9c5ee2fb08a54a3d227442d2
zhaotingting <zhaotingting@oaserver.dw.gdbbk.com>
2017-05-23 14:28:52 +0800

    同步英语(1):下载十本书,统计刷新时间

--- performance-test/src/main/java/com/eebbk/test/performance/SyncEglishTestCase.java
+++ performance-test/src/main/java/com/eebbk/test/performance/SyncEglishTestCase.java
@@ -1,6 +1,7 @@
 package com.eebbk.test.performance;
 
 import android.graphics.Bitmap;
+import android.os.SystemClock;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.uiautomator.By;
 import android.support.test.uiautomator.BySelector;
@@ -19,17 +20,15 @@ import org.junit.runner.RunWith;
 import java.io.IOException;
 import java.util.Date;
 
-import static android.os.SystemClock.sleep;
-
 
 @RunWith(AndroidJUnit4.class)
 public class SyncEglishTestCase extends PerforTestCase {
     BySelector bySelector = By.text("同步英语");
+
     @Test
     public void launchSyncEnglish() throws IOException, UiObjectNotFoundException, InterruptedException, JSONException {
         JSONObject obj = new JSONObject();
-        BySelector bySynEng = By.text("同步英语");
-        Bitmap source_png = getHomeSourceScreen(bySynEng, SyncEnglish.PACKAGE, "imageview_mainbookshelf_blackboard",
+        Bitmap source_png = getHomeSourceScreen(bySelector, SyncEnglish.PACKAGE, "imageview_mainbookshelf_blackboard",
                 5000);
         for (int i = 0; i < mCount; i++) {
             String startScreen = "";
@@ -37,13 +36,11 @@ public class SyncEglishTestCase extends PerforTestCase {
             String compareTime = "";
             int compareResult = 10;
             Date timeStamp1 = new Date();
-
             swipeCurrentLauncher();
-            mDevice.wait(Until.hasObject(bySynEng), WAIT_TIME);
-            UiObject2 synMath = mDevice.findObject(bySynEng);
+            mDevice.wait(Until.hasObject(bySelector), WAIT_TIME);
+            UiObject2 synMath = mDevice.findObject(bySelector);
             startTestRecord();
             synMath.clickAndWait(Until.newWindow(), WAIT_TIME);
-            sleep(1000);
             int m = 0;
             do {
                 m++;
@@ -56,9 +53,7 @@ public class SyncEglishTestCase extends PerforTestCase {
                 if (!des_png.isRecycled()) {
                     des_png.recycle();
                 }
-
                 if ((new Date().getTime() - timeStamp1.getTime()) > WAIT_TIME * 5) {
-                    obj.put("break========:" + String.valueOf(m), compareResult);
                     break;
                 }
             } while (compareResult >= 5);
@@ -79,48 +74,44 @@ public class SyncEglishTestCase extends PerforTestCase {
     @Test
     public void refreshEnglishBook() throws IOException, JSONException, InterruptedException {
         JSONObject obj = new JSONObject();
-        obj.put("refresh:","===========");
         mHelper.openSyncEnglish();
-//        BySelector bySynEng = By.text("英语听说");
-        //com.eebbk.syncenglish:id/refresh
-//        openEnglishTalkHome();
-//        Bitmap source_png = Bitmap.createBitmap(getHomeSourceScreen(bySynEng, EnglishTalk.PACKAGE,
-//                "refresh", 5000), mDevice.getDisplayWidth()
-//                / 2 - 30, mDevice.getDisplayHeight() / 2 - 30, 60, 60);
-//        sleep(5000);
-//        for (int i = 0; i < mCount; i++) {
-//            String startScreen = "";
-//            String endScreen = "";
-//            String compareTime = "";
-//            int compareResult = 10;
-//            Date timeStamp1 = new Date();
-//            startTestRecord();
-//            mDevice.click(mDevice.getDisplayWidth() * 16 / 100, mDevice.getDisplayHeight() * 8 / 100);
-//            int m = 0;
-//            do {
-//                m++;
-//                startScreen = getCurrentDate();
-//                Bitmap des_png = Bitmap.createBitmap(mAutomation.takeScreenshot(), mDevice.getDisplayWidth()
-//                        / 2 - 30, mDevice.getDisplayHeight() / 2 - 30, 60, 60);
-//                endScreen = getCurrentDate();
-//                compareResult = BitmapHelper.compare(source_png, des_png);
-//                compareTime = getCurrentDate();
-//                obj.put("compare:" + String.valueOf(m), compareResult);
-//                if (!des_png.isRecycled()) {
-//                    des_png.recycle();
-//                }
-//                if ((new Date().getTime() - timeStamp1.getTime()) > WAIT_TIME * 5) {
-//                    break;
-//                }
-//            } while (compareResult >= 5);
-//            String loadTime = getCurrentDate();
-//            stopTestRecord(loadTime, startScreen, endScreen, compareTime, String.valueOf(compareResult));
-//            mDevice.waitForIdle();
-//            instrumentationStatusOut(obj);
-//        }
-//        if (!source_png.isRecycled()) {
-//            source_png.recycle();
-//        }
+        Bitmap source_png = mHelper.takeScreenshot(mNumber);
+        UiObject2 refresh = mDevice.findObject(By.res(SyncEnglish.PACKAGE, "refresh"));
+        for (int i = 0; i < mCount; i++) {
+            String startScreen = "";
+            String endScreen = "";
+            String compareTime = "";
+            int compareResult = 10;
+            Date timeStamp1 = new Date();
+            startTestRecord();
+            refresh.clickAndWait(Until.newWindow(), WAIT_TIME);
+            SystemClock.sleep(200);
+            int m = 0;
+            do {
+                m++;
+                startScreen = getCurrentDate();
+                Bitmap des_png = Bitmap.createBitmap(mAutomation.takeScreenshot(), mDevice.getDisplayWidth()
+                        / 2 - 30, mDevice.getDisplayHeight() / 2 - 30, 60, 60);
+                endScreen = getCurrentDate();
+                compareResult = BitmapHelper.compare(Bitmap.createBitmap(source_png, mDevice.getDisplayWidth()
+                        / 2 - 30, mDevice.getDisplayHeight() / 2 - 30, 60, 60), des_png);
+                compareTime = getCurrentDate();
+                obj.put("compare:" + String.valueOf(m), compareResult);
+                if (!des_png.isRecycled()) {
+                    des_png.recycle();
+                }
+                if ((new Date().getTime() - timeStamp1.getTime()) > WAIT_TIME * 2) {
+                    break;
+                }
+            } while (compareResult >= 1);
+            String loadTime = getCurrentDate();
+            stopTestRecord(loadTime, startScreen, endScreen, compareTime, String.valueOf(compareResult));
+            mDevice.waitForIdle();
+            instrumentationStatusOut(obj);
+        }
+        if (!source_png.isRecycled()) {
+            source_png.recycle();
+        }
     }
 
 }
--- test-lib/src/main/java/com/eebbk/test/automator/AutomatorHelper.java
+++ test-lib/src/main/java/com/eebbk/test/automator/AutomatorHelper.java
@@ -1,6 +1,7 @@
 package com.eebbk.test.automator;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.os.RemoteException;
@@ -29,6 +30,8 @@ import com.eebbk.test.common.PackageConstants.Vision;
 import com.eebbk.test.common.PackageConstants.Vtraining;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
@@ -133,6 +136,13 @@ public class AutomatorHelper {
 
     public Bitmap getBitmap(UiObject2 obj) {
         return getBitmap(obj.getVisibleBounds());
+    }
+
+    public Bitmap takeScreenshot(String fileName ) throws FileNotFoundException {
+        mDevice.takeScreenshot(new File("/sdcard/performance-test/" + fileName + "/" + fileName + ".png"));
+        FileInputStream source_fis = new FileInputStream("/sdcard/performance-test/"+fileName+"/"+fileName+".png");
+        Bitmap source_png = BitmapFactory.decodeStream(source_fis);
+        return source_png;
     }
 
     public boolean takeScreenshot(Rect r, File storePath, int quality) {
